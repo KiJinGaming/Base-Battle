3c304036a2e89462766c9cfd37b1de665326cb1c
KiJinGaming <84182232+KiJinGaming@users.noreply.github.com>
2021-12-31 21:09:34 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local url = string.format("https://raw.githubusercontent.com/KiJinGaming/Base-Battle/main/%s", _G.option)
+local url = string.format("https://raw.githubusercontent.com/KiJinGaming/Base-Battle/main/%s.lua", _G.option)
 local f = loadstring(game:HttpGet(url))
 task.spawn(f)
 
